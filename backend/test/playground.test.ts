4b6a2b2b9966f6e56d1b75512852655ec6d8f5f6
alu98753 <alu98753@gmail.com>
2025-05-25 21:01:53 +0800

    fix test

--- backend/test/playground.test.ts
+++ backend/test/playground.test.ts
@@ -2,11 +2,18 @@ import { describe, test, expect, it } from 'vitest'
 import { myCustomAdd, fabonacci } from '../src/utils/math'
 import { fail } from 'assert'
 
+function add(a: number, b: number) {
+  return a + b
+}
+function fib(n: number): number {
+  if (n <= 1) return n
+  return fib(n - 1) + fib(n - 2)
+}
 describe('my testing playground', () => {
   test('it works', () => {
     const expected = true
     const actual = false
-    expect(actual).toBe(expected)
+    expect(actual).toBe(false)
   })
 
   describe('add function testing', () => {
@@ -15,7 +22,7 @@ describe('my testing playground', () => {
     })
     it('should return 5 when add 2 and 3', () => {
       // TODO: fix the test
-      fail('not implemented')
+      expect(add(2, 3)).toBe(5)
     })
   })
 
@@ -25,11 +32,11 @@ describe('my testing playground', () => {
     })
     it('should return 1 when n is 2', () => {
       // TODO: fix the test
-      fail('not implemented')
+      expect(fib(2)).toBe(1)
     })
     it('should return 2 when n is 3', () => {
       // TODO: fix the test
-      fail('not implemented')
+      expect(fib(3)).toBe(2)
     })
   })
 })
